f651798c4a928990e27cdd5528a0a606a94eb935
zacole <10010818@bcs.net.nz>
2017-04-26 09:30:40 +1200

    commit

---
+++ practice2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter in a number");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The number you typed in is mutiplied by 1 = {number * 1}");
+            Console.WriteLine($"The number you typed in is mutiplied by 2 = {number * 2}");
+            Console.WriteLine($"The number you typed in is mutiplied by 3 = {number * 3}");
+            Console.WriteLine($"The number you typed in is mutiplied by 4 = {number * 4}");
+            Console.WriteLine($"The number you typed in is mutiplied by 5 = {number * 5}");
+            Console.WriteLine($"The number you typed in is mutiplied by 6 = {number * 6}");
+            Console.WriteLine($"The number you typed in is mutiplied by 7 = {number * 7}");
+            Console.WriteLine($"The number you typed in is mutiplied by 8 = {number * 8}");
+            Console.WriteLine($"The number you typed in is mutiplied by 9 = {number * 9}");
+            Console.WriteLine($"The number you typed in is mutiplied by 10 = {number * 10}");
+            Console.WriteLine($"The number you typed in is mutiplied by 11 = {number * 11}");
+            Console.WriteLine($"The number you typed in is mutiplied by 12 = {number * 12}");
+        }
+    }
+}
---
+++ practice2/variable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = 6;
+            var num2 = 7;
+            var num3 = 2;
+            var num4 = 3;
+            var num5 = "1";
+            
+            
+            Console.WriteLine($"the value of num1 = {num1} \n");
+            Console.WriteLine($"the value of num2 = {num2}");
+            Console.WriteLine($"the value of num3 = {num3}");
+            Console.WriteLine($"the value of num4 = {num4}");
+            Console.WriteLine($"the value of num5 = {num5}");
+
+
+
+            Console.WriteLine($"num1 + num4 = {num1+num4}");
+            Console.WriteLine($"num5 + num3 = {num5+num3}");
+            Console.WriteLine($"num2 + num3 = {num2+num3}");
+
+
+
+            Console.WriteLine($"question A {(num1+num2)*(num4-num3)}");
+            Console.WriteLine($"Question b {(num1*num2)+(num4*num3)}");
+            Console.WriteLine($"question c {(num1*num2)+ num4*num3}");
+            Console.WriteLine($"question d {(num4*num3)+num1+num2}");
+            Console.WriteLine($"question e {(num4*num3)+num2*num1/num3}");
+            Console.WriteLine($"question f {(num1+num2)*(num4-num3)}");
+            Console.WriteLine($"question g {(num1+num2)*(num4-num3)}");
+            Console.WriteLine($"question k | (6\xb2 * 7) * 3 + 2 = {(Math.Pow(6,2) * 7) + 2)} ");
+        }
+    }
+}
