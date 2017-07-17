b4be3f55630be137b268249796973647ee78bac1
zacole <10010818@bcs.net.nz>
2017-07-18 11:45:13 +1200

    initial commit

---
+++ Project Euler tasks/multiples of 3 and 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace multiples_of_3_and_5
+{
+    class Program
+    {
+    //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    //The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000
+        static void Main(string[] args)
+        {
+        
+            var count = 1000;
+
+            List<int> multiples = new List<int>();    
+            for (var i = 3; i < count; i += 3)
+                {
+                    multiples.Add(i);
+                }
+
+            for (var o = 5; o < count; o += 5)
+                {   
+                    multiples.Add(o);
+                }
+
+            multiples.ForEach(Console.WriteLine);
+            var sum1 = multiples.Sum();
+            Console.WriteLine($"{sum1}");
+
+            
+        }
+
+        
+    }
+}
---
+++ Project Euler tasks/smallest multiple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace smallest_multiple
+{
+    //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 100000000000;
+
+            for(var i = 20; i < count; i --)
+            {
+                if (i = 100000000000)
+
+            }
+        }
+    }
+}
